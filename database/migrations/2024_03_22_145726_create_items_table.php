02ecebac086aab4194b88f7d5b5e42705323a7ca
GrPaladino <paladino.graziano@gmail.com>
2024-03-22 17:22:10 +0100

    Fix: table updated

--- database/migrations/2024_03_22_145726_create_items_table.php
+++ database/migrations/2024_03_22_145726_create_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,7 +15,7 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            // $table->string('description', 150);
+            $table->string('description', 150)->nullable();
             $table->string('slug', 50);
             $table->string('category', 50);
             $table->string('type', 30);
