fea7e5b10635a538c3456e03074753c81d9cc822
davidemarchini <marchini73@gmail.com>
2024-03-22 17:57:51 +0100

    add controller and print table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-})->name('home');
+Route::get('/', [ItemController::class, 'index'])->name('home');
