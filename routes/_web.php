badac815364c342e62b35cd42f283e1844ba0088
GrPaladino <paladino.graziano@gmail.com>
2024-04-03 15:53:36 +0200

    Feat: update auth version

---
+++ routes/_web.php
@@ -0,0 +1,23 @@
+<?php
+use App\Http\Controllers\CharactersController;
+use App\Http\Controllers\ItemController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Rotte tabella Items
+Route::get('/', [ItemController::class, 'index'])->name('home');
+
+
+// Rotte tabella Characters
+Route::resource('characters', CharactersController::class);
