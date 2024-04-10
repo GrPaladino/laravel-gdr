0dbe05dbc09a8085aabd4e2f7dc20459770c7e95
mielecarmine <mielecarmine993@gmail.com>
2024-04-10 17:05:40 +0200

    fix: conflict

--- app/Models/Characters.php
+++ app/Models/Characters.php
@@ -11,13 +11,12 @@ class Characters extends Model
 
     protected $fillable = ['name', 'description', 'attack', 'defence', 'speed', 'life'];
 
-<<<<<<< HEAD
     public function type() {
         return $this->belongsTo(Type::class);
-=======
+    }
+    
     public function items()
     {
         return $this->belongsToMany(Item::class);
->>>>>>> 763f302803f48de39d7c1da99e932cf3e302eabc
     }
 }
