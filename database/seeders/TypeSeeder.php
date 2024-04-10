ff0425c2902f64f8f5497f1d1eaf740a4570a518
mielecarmine <mielecarmine993@gmail.com>
2024-04-10 15:55:23 +0200

    add: seeder Type from csv file

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+       Type::truncate();
+       $csvFile = fopen(base_path("database/data/types.csv"), "r");
+       $firstline = true;
+       while(($data = fgetcsv($csvFile, 2000, ","))!== FALSE) {
+            if(!$firstline) {
+                Type::create([
+                    "name" => $data[0],
+                    "img" => $data[1],
+                    "description" => $data[2]
+                ]);
+            }
+            $firstline = false;
+       }
+       fclose($csvFile); 
     }
 }
